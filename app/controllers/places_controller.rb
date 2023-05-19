b39f7ce6a21bf20f55dd81cf84fb02c6d36307a3
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-05-19 07:06:05 +0000

    reset both controllers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,4 @@
 class PlacesController < ApplicationController
 
-  def index
-    @places = Place.all
-  end
-
-  def new
-  @place = Place.new
-  #   # render posts/new view with new Post form
-  end
-
-  def create
-  #   # start with a new Post
-  @place = Place.new
-  #   # assign user-entered form data to Post's columns
-  @place["name"] = params["place"]["name"]
-
-  #   # save Post row
-  @place.save
-  #   # redirect user
-  redirect_to "/places"
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,5 @@
 class PostsController < ApplicationController
 
-  def index
-    @posts = Post.all
-  end
 
-  def new
-  @post = Post.new
-  #   # render posts/new view with new Post form
-  end
-
-  def create
-  #   # start with a new Post
-  @post = Post.new
-  #   # assign user-entered form data to Post's columns
-  @post["title"] = params["post"]["title"]
-  @post["posted_on"] = params["post"]["posted_on"]
-  @post["place_id"] = params["post"]["place_id"]
-  #   # save Post row
-  @post.save
-  #   # redirect user
-  redirect_to "/posts"
 
 end
