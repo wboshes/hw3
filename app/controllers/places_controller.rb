47dec0737e82e7c5e2e197058015fbc2274204a5
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-05-19 07:13:39 +0000

    Controllers updated

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,33 @@
 class PlacesController < ApplicationController
 
+  def index
+    # find all Company rows
+    @places = Place.all
+    # render companies/index view
+  end
+
+  def show
+    #   # find a Company
+    @place = Place.find_by({ "id" => params["id"] })
+    @posts = Post.where({"place_id" => @place["id"]})
+    #   # render companies/show view with details about Company
+    end
+
+ def new
+  @place = Place.new
+
+    # render view with new Company form
+  end
+  
+  def create
+  #   # start with a new Company
+  @place = Place.new
+  #   # assign user-entered form data to Company's columns
+  @place["name"] = params["place"]["name"]
+  #   # save Company row
+  @place.save
+  #   # redirect user
+  redirect_to "/places"
+  end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
 
+def new
+  @post = Post.new
 
+    # render view with new Company form
+  end
+  
+  def create
+  #   # start with a new Company
+  @post = Post.new
+  #   # assign user-entered form data to Company's columns
+  @post["title"] = params["post"]["title"]
+  @post["description"] = params["post"]["description"]
+  @post["posted_on"] = params["post"]["posted_on"]
+  #   # save Company row
+  @post.save
+  #   # redirect user
+  redirect_to "/posts"
+  end
 
 end
