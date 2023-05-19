ca193a37c9d88fb0852c24f5397f1065d19741ba
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-05-19 16:47:31 +0000

    Next ->  create data and show data scripts

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,17 +7,17 @@ class PlacesController < ApplicationController
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @posts = Post.where({"place_id" => @place["id"] })
-    end
+  end
 
- def new
-  @place = Place.new
+  def new
+   @place = Place.new
   end
   
   def create
-  @place = Place.new
-  @place["name"] = params["place"]["name"]
-  @place.save
-  redirect_to "/places"
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+    redirect_to "/places"
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,12 +14,12 @@ class PostsController < ApplicationController
   end
   
   def create
-  @post = Post.new
-  @post["title"] = params["post"]["title"]
-  @post["posted_on"] = params["post"]["posted_on"]
-  @post["description"] = params["post"]["description"]
-  @post.save
-  redirect_to "/posts"
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["description"] = params["post"]["description"]
+    @post.save
+    redirect_to "/posts"
   end
 
 end
