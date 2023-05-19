534ecad92aacf7c7fa442d4f5bfeaeeac921f2e9
Will Boshes <will.boshes@kellogg.northwestern.edu>
2023-05-19 16:08:21 +0000

    More progress!! posts showing up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find_by({ "id" => params["id"] })
     @place = Place.find_by({ "id" => @post["company_id"] })
@@ -7,21 +11,16 @@ class PostsController < ApplicationController
   
   def new
   @post = Post.new
-
-    # render view with new Company form
   end
   
   def create
-  #   # start with a new Company
   @post = Post.new
-  #   # assign user-entered form data to Company's columns
   @post["title"] = params["post"]["title"]
-  @post["description"] = params["post"]["description"]
   @post["posted_on"] = params["post"]["posted_on"]
-  #   # save Company row
+  @post["description"] = params["post"]["description"]
   @post.save
-  #   # redirect user
   redirect_to "/posts"
   end
-
+  
 end
+
